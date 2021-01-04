f84d8b62c8ba3ea02d82711e9a0b281d435da259
Sebastian Zawołański <sebastol90@wp.pl>
2021-01-04 18:18:15 +0100

    feat: change data in views

--- ships.js
+++ ships.js
@@ -10,8 +10,29 @@ const changeActiveLink = (activeEl) => {
 };
 
 const setCurrentViewData = (rockets, hash) => {
-  const rocket = rockets.filter((r) => r.name.replace(/\s/g, '').toLowerCase() === hash);
+  const [rocket] = rockets.filter((r) => r.name.replace(/\s/g, '').toLowerCase() === hash);
+
   console.log(rocket);
+
+  const title = document.querySelector('.section__title');
+  const status = document.querySelector('.section__subtitle > span');
+  const desc = document.querySelector('.section__description');
+  const height = document.querySelector('.overview__list__element__value--height');
+  const diameter = document.querySelector('.overview__list__element__value--diameter');
+  const mass = document.querySelector('.overview__list__element__value--mass');
+  const leo = document.querySelector('.overview__list__element__value--leo');
+  const stages = document.querySelector('.overview__list__element__value--stages');
+  const firstflight = document.querySelector('.overview__list__element__value--firstflight');
+
+  title.innerText = rocket.name;
+  status.innerText = rocket.status ? 'active' : 'inactive';
+  desc.innerText = rocket.description;
+  height.innerText = `${rocket.height.meters} m`;
+  diameter.innerText = `${rocket.diameter.meters} m`;
+  mass.innerText = `${rocket.mass.kg} kg`;
+  leo.innerText = `${rocket.payload_weights[0].kg} kg`;
+  stages.innerText = rocket.stages;
+  firstflight.innerText = rocket.first_flight;
 };
 
 const changeView = (e, rockets) => {
