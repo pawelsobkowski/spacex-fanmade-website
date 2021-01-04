c790b417fd80ced42c049206f3b5d9163202cfb7
Sebastian Zawołański <sebastol90@wp.pl>
2021-01-04 17:48:13 +0100

    feat: create change active link function

--- ships.js
+++ ships.js
@@ -1,13 +1,23 @@
-const getHash = (e) => e.target.href.split('#')[1];
+const getHash = (el) => el.href.split('#')[1];
 
-const changeView = (e) => {
-  const hash = getHash(e);
-  console.log(hash);
+const getElFromHash = (hash, links) =>
+  [...document.querySelectorAll('.nav__list__element > a')].filter((el) => getHash(el) === hash);
+
+const changeActiveLink = (activeEl) => {
+  li = document.querySelectorAll('.nav__list__element');
+  li.forEach((el) => el.classList.remove('nav__list__element--active'));
+  activeEl.parentNode.classList.add('nav__list__element--active');
 };
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  const hash = window.location.hash;
+const changeView = (e, links) => {
+  const hash = getHash(e.target);
+  changeActiveLink(e.target);
+};
 
-  const a = document.querySelectorAll('.nav__list__element > a');
-  a.forEach((el) => el.addEventListener('click', changeView));
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('.nav__list__element > a');
+  const hash = window.location.hash.split('#')[1];
+  changeActiveLink(getElFromHash(hash, links)[0]);
+
+  links.forEach((el) => el.addEventListener('click', (e) => changeView(e, links)));
 });
