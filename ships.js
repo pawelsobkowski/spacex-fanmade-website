9d9e955efa81a75362b4e2521de67bcfd636878b
Sebastian Zawołański <sebastol90@wp.pl>
2021-01-04 18:05:08 +0100

    feat: fetch rockets data

--- ships.js
+++ ships.js
@@ -1,6 +1,6 @@
 const getHash = (el) => el.href.split('#')[1];
 
-const getElFromHash = (hash, links) =>
+const getElFromHash = (hash) =>
   [...document.querySelectorAll('.nav__list__element > a')].filter((el) => getHash(el) === hash);
 
 const changeActiveLink = (activeEl) => {
@@ -9,15 +9,32 @@ const changeActiveLink = (activeEl) => {
   activeEl.parentNode.classList.add('nav__list__element--active');
 };
 
-const changeView = (e, links) => {
-  const hash = getHash(e.target);
-  changeActiveLink(e.target);
+const setCurrentViewData = (rockets, hash) => {
+  const rocket = rockets.filter((r) => r.name.replace(/\s/g, '').toLowerCase() === hash);
+  console.log(rocket);
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+const changeView = (e, rockets) => {
+  const hash = getHash(e.target);
+  changeActiveLink(e.target);
+  setCurrentViewData(rockets, hash);
+};
+
+const fetchRockets = () => fetch('https://api.spacexdata.com/v4/rockets');
+
+document.addEventListener('DOMContentLoaded', async () => {
+  let rockets;
   const links = document.querySelectorAll('.nav__list__element > a');
   const hash = window.location.hash.split('#')[1];
-  changeActiveLink(getElFromHash(hash, links)[0]);
+  changeActiveLink(getElFromHash(hash)[0]);
+  try {
+    const data = await fetchRockets();
+    rockets = await data.json();
+    setCurrentViewData(rockets, hash);
+    console.log(rockets);
+  } catch (e) {
+    console.log(e);
+  }
 
-  links.forEach((el) => el.addEventListener('click', (e) => changeView(e, links)));
+  links.forEach((el) => el.addEventListener('click', (e) => changeView(e, rockets)));
 });
